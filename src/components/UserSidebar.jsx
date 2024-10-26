1f657a785d2ef3978dda4ffb6da0efeac283715c
Zacharovanii <tuzudavid7@gmail.com>
2024-10-26 19:34:40 +0300

    remade project add: register, login, main pages

---
+++ src/components/UserSidebar.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function UserSidebar() {
+	return (
+		<div className="sidebar">
+			<img src="../styles/avaava.jpg" alt="user_photo" />
+			<h1>Иван Иванов Иванович</h1>
+		</div>
+	)
+}
+
+export default UserSidebar
