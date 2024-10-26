7eb726513035b0619417d1b294f89680b6bca82f
Zacharovanii <tuzudavid7@gmail.com>
2024-10-26 11:27:01 +0300

    almost done login design

--- src/Components/LoginForm.jsx
+++ src/Components/LoginForm.jsx
@@ -4,19 +4,39 @@ import "./LoginForm.module.css"
 
 function LoginForm() {
 	const [login, setLogin] = useState("")
+	const [pass, setPassword] = useState("")
+
+	function checkUser() {
+		pass
+	}
 
 	return (
 		<Glass>
-			<form action="" method="get">
+			<form method="get">
 				<div className="head">
-					<h3>Войти</h3>
+					<h3>Авторизироваться</h3>
 				</div>
 
 				<div className="login-fields">
 					<label htmlFor="login">Логин</label>
-					<input type="text" id="login" />
-
+					<input
+						type="text"
+						id="login"
+						onChange={e => setLogin(e.target.value)}
+					/>
+					<label htmlFor="password">Пароль</label>
+					<input
+						type="password"
+						id="login"
+						onChange={e => setPassword(e.target.value)}
+					/>
 				</div>
+				<button
+					type='submit'
+					onClick={checkUser}
+				>
+					Войти
+				</button>
 			</form>
 		</Glass>
 	)
